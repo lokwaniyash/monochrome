49a1ef8da071ae115fed44c158b3708eece19a76
Yash Lokwani <lokwaniyash2000@gmail.com>
2026-02-22 03:13:19 +0530

    chore: added domain to vite config allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => {
             fs: {
                 allow: ['.', 'node_modules'],
             },
+            allowedHosts: ['localhost', 'monochrome.24122000.xyz'],
         },
         build: {
             outDir: 'dist',
